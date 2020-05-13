402bbda10ad41185c7fd0dc7503d13dc1f620704
pradeepdhiren <pradeepdhiren>
2020-05-13 13:07:57 +0530

    AWS BeanStalk example initial commit

---
+++ src/test/java/com/techprimers/com/aws/AwsElasticBeanstalkExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.com.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
